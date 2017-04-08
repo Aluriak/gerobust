900b9899db8074df1ab9dcacf77de1b31f3e185d
Aluriak <lucas.bourneuf@laposte.net>
2017-04-08 10:18:58 +0200

    fix doc spaces

--- gerobust/wrapper.py
+++ gerobust/wrapper.py
@@ -77,9 +77,9 @@ def orientation_3d_fast(pa:(float, float), pb:(float, float),
     Return a positive value if the point pd lies below the
     plane passing through pa, pb, and pc; "below" is defined so
     that pa, pb, and pc appear in counterclockwise order when
-    viewed from above the plane.  Returns a negative value if
-    pd lies above the plane.  Returns zero if the points are
-    coplanar.  The result is also a rough approximation of six
+    viewed from above the plane. Returns a negative value if
+    pd lies above the plane. Returns zero if the points are
+    coplanar. The result is also a rough approximation of six
     times the signed volume of the tetrahedron defined by the
     four points.
 
@@ -91,14 +91,14 @@ def orientation_3d_fast(pa:(float, float), pb:(float, float),
 
 def orientation_3d(pa:(float, float), pb:(float, float),
                    pc:(float, float), pd:(float, float)) -> float:
-    """Adaptive exact 3D orientation test.  Robust.
+    """Adaptive exact 3D orientation test. Robust.
 
     Return a positive value if the point pd lies below the
     plane passing through pa, pb, and pc; "below" is defined so
     that pa, pb, and pc appear in counterclockwise order when
-    viewed from above the plane.  Returns a negative value if
-    pd lies above the plane.  Returns zero if the points are
-    coplanar.  The result is also a rough approximation of six
+    viewed from above the plane. Returns a negative value if
+    pd lies above the plane. Returns zero if the points are
+    coplanar. The result is also a rough approximation of six
     times the signed volume of the tetrahedron defined by the
     four points.
 
@@ -174,12 +174,12 @@ def inspherefast(pa:(float, float), pb:(float, float), pc:(float, float),
 
 def insphere(pa:(float, float), pb:(float, float), pc:(float, float),
              pd:(float, float), pe:(float, float)) -> float:
-    """Adaptive exact 3D insphere test.  Robust.
+    """Adaptive exact 3D insphere test. Robust.
 
     Return a positive value if the point pe lies inside the
     sphere passing through pa, pb, pc, and pd; a negative value
     if it lies outside; and zero if the five points are
-    cospherical.  The points pa, pb, pc, and pd must be ordered
+    cospherical. The points pa, pb, pc, and pd must be ordered
     so that they have a positive orientation (as defined by
     orient3d()), or the sign of the result will be reversed.
 
