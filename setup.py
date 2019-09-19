23a9772a29c9e152f6830278ff54183a70851ac2
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2019-09-19 13:22:49 +0200

    update for pip 10

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import os
 from setuptools   import setup, find_packages
-from pip.req      import parse_requirements
-from pip.download import PipSession
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+    from pip._internal.download import PipSession
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    from pip.download import PipSession
 
 
 # import the data inside package/info.py file, without trigger the
